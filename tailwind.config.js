09d85ff2eeabc61cd924af9c1ec80d05d955cd2a
Enis Tufekci <enistfkcii@gmail.com>
2023-12-19 13:47:05 +0300

    Fontlar ve renkler

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container : {
+      center:true
+    },
+    extend: {
+      fontFamily: {
+        gemunu : ['Gemunu Libre','sans-serif'],
+        open   : ['Open Sans','sans-serif'],
+      },
+      colors:{
+        'origin-red' : '#BC1A45',
+        'origin-melon' : '#FFD369',
+        'origin-grey' : '#DDDDDD',
+        'origin-white' : '#F7F7F7',
+       },
+    },
   },
   plugins: [],
 }
